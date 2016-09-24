135c9f231639ee4873882c18cbaa4d8e19546e71
priteshrnandgaonkar <pncooldude91@gmail.com>
2016-09-24 20:49:49 +0530

    Initial Commit

---
+++ CardsStack/CardsStack.h
@@ -0,0 +1,19 @@
+//
+//  CardsStack.h
+//  CardsStack
+//
+//  Created by housing on 9/24/16.
+//  Copyright © 2016 Pritesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CardsStack.
+FOUNDATION_EXPORT double CardsStackVersionNumber;
+
+//! Project version string for CardsStack.
+FOUNDATION_EXPORT const unsigned char CardsStackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardsStack/PublicHeader.h>
+
+
