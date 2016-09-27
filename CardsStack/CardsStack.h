1ad1d6ed0f45bfa30c3978bc921b30b9497a046d
priteshrnandgaonkar <pncooldude91@gmail.com>
2016-09-27 15:49:29 +0530

    Solved the error while building

--- CardsStack/CardsStack.h
+++ CardsStack/CardsStack.h
@@ -15,6 +15,4 @@ FOUNDATION_EXPORT double CardsStackVersionNumber;
 FOUNDATION_EXPORT const unsigned char CardsStackVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CardsStack/PublicHeader.h>
-#import <CardsStack/CardLayout.h>
-#import <CardsStack/CardStack.h>
 
